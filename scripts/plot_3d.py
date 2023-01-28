0ffad3aabe664f0b161a168a6d1e7d8efc65ce11
Benjamin Bolte <ben@bolte.cc>
2023-01-27 20:07:17 -0500

    changes to 3d plotting script

--- scripts/plot_3d.py
+++ scripts/plot_3d.py
@@ -4,12 +4,14 @@ from typing import List, Union
 
 import matplotlib.pyplot as plt
 
-from tmesh import Line3D, Point3D, Triangle3D, Trimesh3D
+from tmesh import Line3D, Point3D, Tetrahedron3D, Triangle3D, Trimesh3D
 
-Thing = Union[Line3D, Point3D, Triangle3D, Trimesh3D]
+Thing = Union[Line3D, Point3D, Tetrahedron3D, Triangle3D, Trimesh3D]
 
 # To plot things, add them to this list.
 THINGS_TO_PLOT: List[Thing] = []
+
+ALPHA = 1.0
 
 
 def get_color(i: int) -> str:
@@ -35,7 +37,7 @@ def plot_line(axes: plt.Axes, line: Line3D, i: int) -> None:
         i: Index of line.
     """
 
-    axes.plot3D([line.p1.x, line.p2.x], [line.p1.y, line.p2.y], [line.p1.z, line.p2.z], get_color(i))
+    axes.plot3D([line.p1.x, line.p2.x], [line.p1.y, line.p2.y], [line.p1.z, line.p2.z], get_color(i), alpha=ALPHA)
 
 
 def plot_point(axes: plt.Axes, point: Point3D, i: int) -> None:
@@ -47,7 +49,7 @@ def plot_point(axes: plt.Axes, point: Point3D, i: int) -> None:
         i: Index of point.
     """
 
-    axes.plot3D(point.x, point.y, point.z, get_color(i) + "o")
+    axes.plot3D(point.x, point.y, point.z, get_color(i) + "o", alpha=ALPHA)
 
 
 def plot_triangle(axes: plt.Axes, triangle: Triangle3D, i: int) -> None:
@@ -64,7 +66,21 @@ def plot_triangle(axes: plt.Axes, triangle: Triangle3D, i: int) -> None:
         [triangle.p1.y, triangle.p2.y, triangle.p3.y, triangle.p1.y],
         [triangle.p1.z, triangle.p2.z, triangle.p3.z, triangle.p1.z],
         get_color(i),
+        alpha=ALPHA,
     )
+
+
+def plot_tetrahedron(axes: plt.Axes, tetrahedron: Tetrahedron3D, i: int) -> None:
+    """Plots a tetrahedron.
+
+    Args:
+        axes: Axes to plot on.
+        tetrahedron: Tetrahedron to plot.
+        i: Index of tetrahedron.
+    """
+
+    for face in tetrahedron.faces:
+        plot_triangle(axes, face, i)
 
 
 def plot_trimesh(axes: plt.Axes, trimesh: Trimesh3D, i: int) -> None:
@@ -98,6 +114,8 @@ def main() -> None:
             plot_triangle(axes, thing, i)
         elif isinstance(thing, Trimesh3D):
             plot_trimesh(axes, thing, i)
+        elif isinstance(thing, Tetrahedron3D):
+            plot_tetrahedron(axes, thing, i)
         else:
             raise ValueError(f"Unknown thing to plot: {thing}")
 
