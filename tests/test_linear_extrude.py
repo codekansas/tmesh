b389467b2d9fefdee82b98510fb6c2d2ea474215
Benjamin Bolte <ben@bolte.cc>
2023-02-24 01:07:07 -0500

    update testS

--- tests/test_linear_extrude.py
+++ tests/test_linear_extrude.py
@@ -10,8 +10,12 @@ from tmesh import linear_extrude, regular_polygon
 def test_simple_linear_extrude() -> None:
     """Tests extruding a triangle to a tetrahedron."""
 
+    height = 2.0
+
     mesh = regular_polygon(1.0, n=3).get_trimesh()
-    extruded_mesh = linear_extrude(mesh, 1.0)
+    extruded_mesh = linear_extrude(mesh, height)
+
+    exp_volume = sum(t.area() for t in mesh.get_triangles()) * height
 
     assert len(extruded_mesh.vertices) == 6
     assert len(extruded_mesh.volumes) == 3
@@ -19,7 +23,8 @@ def test_simple_linear_extrude() -> None:
     # Checks that the tetrahedrons are all oriented correctly and have the
     # expected volume.
     for tetra in extruded_mesh.get_tetraherdons():
-        assert tetra.signed_volume() == pytest.approx(math.sqrt(3) / 4)
+        assert tetra.signed_volume() == pytest.approx(height * math.sqrt(3) / 4)
+    assert sum(t.signed_volume() for t in extruded_mesh.get_tetraherdons()) == pytest.approx(exp_volume)
 
     # Checks the triangular mesh.
     trimesh = extruded_mesh.to_trimesh()
--- tests/test_types.py
+++ tests/test_types.py
@@ -1,20 +1,46 @@
+"""Tests types which are shared between two and three dimensions."""
+
+import itertools
 import random
 
 from tmesh import Face, Volume
 
 
 def test_face_hashing() -> None:
-    get_face = lambda v: Face(*(random.randint(0, v) for _ in range(3)))
+    inds = list(range(100))
+    get_face = lambda v: Face(*random.sample(inds, k=3))
     faces = {get_face(100) for _ in range(100)}
-    for face in faces:
-        assert Face(face.b, face.c, face.a) in faces
-        assert Face(face.c, face.a, face.b) in faces
+    for f in faces:
+        valid_permutations = [
+            Face(f.a, f.b, f.c),
+            Face(f.c, f.a, f.b),
+            Face(f.b, f.c, f.a),
+        ]
+
+        for a, b, c in itertools.permutations([f.a, f.b, f.c], r=3):
+            f2 = Face(a, b, c)
+            if f2 in valid_permutations:
+                assert f2 in faces
+            else:
+                assert f2 not in faces
 
 
 def test_volume_hashing() -> None:
-    get_volume = lambda v: Volume(*(random.randint(0, v) for _ in range(4)))
+    inds = list(range(100))
+    get_volume = lambda v: Volume(*random.sample(inds, k=4))
     volumes = {get_volume(100) for _ in range(100)}
-    for volume in volumes:
-        assert Volume(volume.b, volume.c, volume.d, volume.a) in volumes
-        assert Volume(volume.c, volume.d, volume.a, volume.b) in volumes
-        assert Volume(volume.d, volume.a, volume.b, volume.c) in volumes
+
+    for v in volumes:
+        valid_permutations = [
+            Volume(v.a, v.b, v.c, v.d),
+            Volume(v.d, v.a, v.b, v.c),
+            Volume(v.c, v.d, v.a, v.b),
+            Volume(v.b, v.c, v.d, v.a),
+        ]
+
+        for a, b, c, d in itertools.permutations([v.a, v.b, v.c, v.d], r=4):
+            v2 = Volume(a, b, c, d)
+            if v2 in valid_permutations:
+                assert v2 in volumes
+            else:
+                assert v2 not in volumes
